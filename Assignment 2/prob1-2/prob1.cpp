4cb87e2fe2d0460d2ec1f93543c81e941c3c86f7
Yash Gupta <yashbg7@gmail.com>
2023-09-10 21:52:07 +0530

    Complete producer()

--- Assignment 2/prob1-2/prob1.cpp
+++ Assignment 2/prob1-2/prob1.cpp
@@ -9,9 +9,23 @@
 
 std::multiset<std::string> x;
 std::queue<std::string> y;
+pthread_mutex_t x_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t y_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *producer(void *arg) {
-    
+    pthread_mutex_lock(&x_mutex);
+    std::string file = *x.begin();
+    x.erase(x.begin());
+    pthread_mutex_unlock(&x_mutex);
+
+    std::ifstream ifs(file);
+    std::string line;
+
+    while (getline(ifs, line)) {
+        pthread_mutex_lock(&y_mutex);
+        y.push(line);
+        pthread_mutex_unlock(&y_mutex);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -19,9 +33,9 @@ int main(int argc, char *argv[]) {
     std::string input(argv[1]);
 
     std::ifstream ifs(input);
-    std::string line;
-    while (ifs >> line) {
-        x.insert(line);
+    std::string file;
+    while (ifs >> file) {
+        x.insert(file);
     }
 
     int n = x.size();
