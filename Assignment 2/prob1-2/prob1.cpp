6a02ae3fc2d8f2f001d2ffe8d7e9e3b66e7030dc
Yash Gupta <yashbg7@gmail.com>
2023-09-10 20:48:10 +0530

    Add x

--- Assignment 2/prob1-2/prob1.cpp
+++ Assignment 2/prob1-2/prob1.cpp
@@ -3,17 +3,21 @@
 #include <string>
 #include <cassert>
 #include <fstream>
+#include <set>
+
+std::multiset<std::string> x;
 
 int main(int argc, char *argv[]) {
     assert(argc == 2);
     std::string input(argv[1]);
-    std::vector<std::string> lines;
 
     std::ifstream ifs(input);
     std::string line;
     while (ifs >> line) {
-        lines.push_back(line);
+        x.insert(line);
     }
+
+    int n = x.size();
 
     return 0;
 }
