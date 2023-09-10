a7c2c72bf05bd59709827908190af1033c26ca2e
Yash Gupta <yashbg7@gmail.com>
2023-09-10 20:38:50 +0530

    Add input

--- Assignment 2/prob1-2/prob1.cpp
+++ Assignment 2/prob1-2/prob1.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <cassert>
+#include <fstream>
 
 int main(int argc, char *argv[]) {
-    char *input = argv[1];
+    assert(argc == 2);
+    std::string input(argv[1]);
+    std::vector<std::string> lines;
+
+    std::ifstream ifs(input);
+    std::string line;
+    while (ifs >> line) {
+        lines.push_back(line);
+    }
 
     return 0;
 }
