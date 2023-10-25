985bb59e573d3a60d1bff62af162d2f43176b976
Yash Gupta <yashbg@iitk.ac.in>
2023-10-25 22:36:36 +0530

    Add kernel1()

--- assignment4/problem1.cu
+++ assignment4/problem1.cu
@@ -8,6 +8,7 @@
 
 const uint64_t N = (64);
 #define THRESHOLD (0.000001)
+const uint64_t MAX_VAL = 1e6;
 
 using std::cerr;
 using std::cout;
@@ -23,8 +24,20 @@ inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort =
   }
 }
 
-// TODO: Edit the function definition as required
-__global__ void kernel1() {}
+__global__ void kernel1(const double *in, double *out) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  int k = blockIdx.z * blockDim.z + threadIdx.z;
+
+  if (i > 0 && i < N - 1 && j > 0 && j < N - 1 && k > 0 && k < N - 1) {
+    out[i * N * N + j * N + k] = 0.8 * (in[(i - 1) * N * N + j * N + k]
+                                      + in[(i + 1) * N * N + j * N + k]
+                                      + in[i * N * N + (j - 1) * N + k]
+                                      + in[i * N * N + (j + 1) * N + k]
+                                      + in[i * N * N + j * N + (k - 1)]
+                                      + in[i * N * N + j * N + (k + 1)]);
+  }
+}
 
 // TODO: Edit the function definition as required
 __global__ void kernel2() {}
@@ -97,25 +110,53 @@ int main() {
   uint64_t SIZE = N * N * N;
 
   double *h_in = static_cast<double *>(malloc(SIZE * sizeof(double)));
+  double *h_out_serial = static_cast<double *>(malloc(SIZE * sizeof(double)));
   double *h_out = static_cast<double *>(malloc(SIZE * sizeof(double)));
 
+  for (int i = 0; i < SIZE; i++) {
+    h_in[i] = rand() % MAX_VAL;
+    h_out_serial[i] = 0.0;
+    h_out[i] = 0.0;
+  }
+
   double clkbegin = rtclock();
-  stencil(h_in, h_out);
+  stencil(h_in, h_out_serial);
   double clkend = rtclock();
   double cpu_time = clkend - clkbegin;
   cout << "Stencil time on CPU: " << cpu_time * 1000 << " msec" << endl;
 
-  cudaError_t status;
+  double *d_in;
+  cudaCheckError(cudaMalloc(&d_in, SIZE * sizeof(double)));
+  double *d_out;
+  cudaCheckError(cudaMalloc(&d_out, SIZE * sizeof(double)));
+
+  cudaCheckError(cudaMemcpy(d_in, h_in, SIZE * sizeof(double),
+                            cudaMemcpyHostToDevice));
+
   cudaEvent_t start, end;
-  // TODO: Fill in kernel1
-  // TODO: Adapt check_result() and invoke
+  cudaCheckError(cudaEventCreate(&start));
+  cudaCheckError(cudaEventCreate(&end));
+  cudaCheckError(cudaEventRecord(start));
+
+  dim3 threadsPerBlock(1, 32, 32);
+  dim3 numBlocks(N / threadsPerBlock.x, N / threadsPerBlock.y, N / threadsPerBlock.z);
+  kernel1<<<numBlocks, threadsPerBlock>>>(d_in, d_out);
+  
+  cudaCheckError(cudaEventRecord(end));
+  cudaCheckError(cudaEventSynchronize(end));
+
+  cudaCheckError(cudaMemcpy(h_out, d_out, SIZE * sizeof(double),
+                            cudaMemcpyDeviceToHost));
+
+  check_result(h_out_serial, h_out, N);
+
   float kernel_time;
-  cudaEventElapsedTime(&kernel_time, start, end);
+  cudaCheckError(cudaEventElapsedTime(&kernel_time, start, end));
   std::cout << "Kernel 1 time (ms): " << kernel_time << "\n";
 
   // TODO: Fill in kernel2
   // TODO: Adapt check_result() and invoke
-  cudaEventElapsedTime(&kernel_time, start, end);
+  cudaCheckError(cudaEventElapsedTime(&kernel_time, start, end));
   std::cout << "Kernel 2 time (ms): " << kernel_time << "\n";
 
   // TODO: Free memory
